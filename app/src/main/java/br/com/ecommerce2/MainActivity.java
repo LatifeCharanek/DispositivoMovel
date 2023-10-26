71221a2b9efd8287cfd8ac39c260a65917ba1a31
Latifi Charanek <111158217+fifizika@users.noreply.github.com>
2023-10-25 22:08:52 -0300

    Login

--- app/src/main/java/br/com/ecommerce2/MainActivity.java
+++ app/src/main/java/br/com/ecommerce2/MainActivity.java
@@ -4,7 +4,6 @@ import static br.com.ecommerce2.R.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -14,18 +13,30 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-    private TextView textInfo;
+   // private TextView textInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
 
+        //textInfo = findViewById(id.TestInfo2);
+        //textInfo.setText("Boa noite a todos!");
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.TestInfo2.setText("Olá Mundo sz");
+        binding.TestInfo2.setText("Olá desenvolvedor sz");
 
 
+    }
+
+    public static class SegActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(layout.activity_seg2);
+        }
     }
 }
---
+++ app/src/main/java/br/com/ecommerce2/SegActivity2.java
@@ -0,0 +1,14 @@
+package br.com.ecommerce2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seg3);
+    }
+}
